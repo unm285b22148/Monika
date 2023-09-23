9dabe0f2bc5e0e3b29337637b0c895d47ee2ffee
unm285b22148 <145859437+unm285b22148@users.noreply.github.com>
2023-09-23 21:34:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def linear_search_product(product_list, target_product):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(index)
+    return indices
+
+# Example usage
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+target_product = "Apple"
+
+result = linear_search_product(products, target_product)
+if result:
+    print(f"{target_product} found at indices: {result}")
+else:
+    print(f"{target_product} not found in the list.")
