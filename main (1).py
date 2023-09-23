9bc7eb80fa1844e1e8b5e67d51c12f68efa3619c
unm285b22148 <145859437+unm285b22148@users.noreply.github.com>
2023-09-23 21:33:10 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the student list based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Example usage
+student1 = Student("Alice", "A101", 3.8)
+student2 = Student("Bob", "B102", 3.5)
+student3 = Student("Charlie", "C103", 4.0)
+
+students = [student1, student2, student3]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
